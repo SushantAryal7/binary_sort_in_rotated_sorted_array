8d2cabb92021150a6b61e56b48ed550b64cf4c14
sushant Aryal <aryasushant7@gmail.com>
2024-05-03 11:02:20 +0530

    Create binary_search.js

---
+++ binary_search.js
@@ -0,0 +1,32 @@
+console.log('jai bhole nath ki jai ho');
+
+function binary_search(arr, target){
+    let high = arr.length;
+    let low = 0 ;
+    while(high>=low){
+        let mid = Math.abs((high + low)/2)
+
+        if(arr[mid] == target){
+            return mid;
+        }
+        if(arr[mid] > arr[low]){
+            if(arr[mid] > target && target>= arr[low]){
+                high = mid-1 ;
+            }else{
+                low = mid+1 ;
+            }
+        }
+        else{
+            if(arr[mid]< target && target <=arr[high]){
+                low = mid+1 ;
+            }
+            else{
+                high = mid -1 ;
+            }
+
+        }
+    }
+}
+let arr = [4, 5, 6, 7, 0, 2];
+let target = 0;
+console.log(binary_search(arr, target));
